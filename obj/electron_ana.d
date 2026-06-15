e62e78205fce8e9f2ac3f58083886a70628fe2f7
Andrew Grieder <acgrieder@wisc.edu>
2026-06-15 14:27:38 -0500

    add initial files

---
+++ obj/electron_ana.d
@@ -0,0 +1,34 @@
+obj/electron_ana.o: src_v4.5.7/electron_ana.cpp src_v4.5.7/electron.h \
+ src_v4.5.7/common_headers.h src_v4.5.7/comm/ODE.h \
+ src_v4.5.7/comm/scalar.h src_v4.5.7/comm/mymp.h \
+ src_v4.5.7/comm/matrix3.h src_v4.5.7/comm/vector3.h \
+ src_v4.5.7/comm/Units.h src_v4.5.7/comm/myio.h \
+ src_v4.5.7/comm/constants.h src_v4.5.7/comm/myarray.h \
+ src_v4.5.7/comm/Random.h src_v4.5.7/comm/mymatrix.h \
+ src_v4.5.7/comm/sparse_matrix.h src_v4.5.7/comm/sparse2D.h \
+ src_v4.5.7/comm/Histogram.h src_v4.5.7/comm/GaussianSmapling.h \
+ src_v4.5.7/comm/kmap.h src_v4.5.7/comm/vector3.h src_v4.5.7/lattice.h \
+ src_v4.5.7/parameters.h src_v4.5.7/PumpProbe.h src_v4.5.7/Scatt_Param.h
+src_v4.5.7/electron.h:
+src_v4.5.7/common_headers.h:
+src_v4.5.7/comm/ODE.h:
+src_v4.5.7/comm/scalar.h:
+src_v4.5.7/comm/mymp.h:
+src_v4.5.7/comm/matrix3.h:
+src_v4.5.7/comm/vector3.h:
+src_v4.5.7/comm/Units.h:
+src_v4.5.7/comm/myio.h:
+src_v4.5.7/comm/constants.h:
+src_v4.5.7/comm/myarray.h:
+src_v4.5.7/comm/Random.h:
+src_v4.5.7/comm/mymatrix.h:
+src_v4.5.7/comm/sparse_matrix.h:
+src_v4.5.7/comm/sparse2D.h:
+src_v4.5.7/comm/Histogram.h:
+src_v4.5.7/comm/GaussianSmapling.h:
+src_v4.5.7/comm/kmap.h:
+src_v4.5.7/comm/vector3.h:
+src_v4.5.7/lattice.h:
+src_v4.5.7/parameters.h:
+src_v4.5.7/PumpProbe.h:
+src_v4.5.7/Scatt_Param.h:
